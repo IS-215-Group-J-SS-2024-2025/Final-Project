25b1b0732a696055f93c8fc6ba376efb2f85a746
mrservo-up <mrservo@up.edu.ph>
2025-04-29 10:19:05 +0000

    working finally!!!

--- index.js
+++ index.js
@@ -5,24 +5,35 @@ const S3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 
-const S3_BUCKET = BUCKET;
-const S3_URL = URL;
-const REGION_NAME = 'us-east-1';
-
-const mime = require("mime-types");
-const unzip = require("unzipper");
+// MAKE SURE THAT ENVIRONMENT VARIABLES ARE SET
+const S3_BUCKET = process.env.BUCKET;
+const S3_URL = process.env.URL;
+const IAM_ROLE = process.env.ROLE_ARN; 
 
 exports.handler = function(event, context, callback) {
     const key = event.Records[0].s3.object.key;
+    if (key.indexOf('outs/') >= 0) {
+      // DO NOT include for analysis
+      // those files inside the outs folder
+      return;
+    }
+  
     S3.getObject({Bucket: S3_BUCKET, Key: key}).promise()
       .then((data) => {
-        // process rekognition analysis here
-        // and return 
 
-        var creds = new AWS.SharedIniFileCredentials({});
+        // USE ASSUMED ROLE OF LAMBDA AND S3 ACCOUNTS
+        const sts = new AWS.STS();
+        const role_params = {
+            RoleArn: IAM_ROLE,
+            RoleSessionName: 'LambdaAssumeRoleSession'
+        };
+        const data_role = sts.assumeRole(role_params).promise();
+        const creds = data_role.Credentials;
+
+        // TRANSFER CREDENTIALS FOR THE API FUNCTION CALL
         AWS.config.credentials = creds;
-        AWS.config.update({region:REGION_NAME});
 
+        // PREPARE API PARAMETERS
         const client = new AWS.Rekognition();
         const params = {
           Image: {
@@ -34,12 +45,13 @@ exports.handler = function(event, context, callback) {
           MaxLabels: 10
         }
 
+        // CALL API AND FORMAT RESULT
         client.detectLabels(params, function(err, response) {
           var buffer = "START;";
           if (err) {
             buffer += err + `<br/>` + err.stack; // if an error occurred
           } else {
-            buffer += `Detected labels for: ${photo}` + `<br/>`;
+            buffer += `Detected labels for: ${key}` + `<br/>`;
             response.Labels.forEach(label => {
               buffer += `Label:      ${label.Name}` + `<br/>`;
               buffer += `Confidence: ${label.Confidence}`+ `<br/>`;
@@ -65,7 +77,7 @@ exports.handler = function(event, context, callback) {
             Body: buffer,
             Bucket: S3_BUCKET,
             ContentType: "text/html; charset=utf-8",
-            Key: 'outs/${key}',
+            Key: `outs/${key}.txt`,
           }).promise()
         });
       })
